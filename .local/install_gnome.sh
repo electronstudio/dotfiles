85a5e173f866c140ca03279de2f291f11f5a4b72
Richard Smith <github@electronstudio.co.uk>
2025-11-11 13:59:23 +0000

    install gnome

---
+++ .local/install_gnome.sh
@@ -0,0 +1,6 @@
+uv tool install gnome-extensions-cli
+gext install windowIsReady_Remover@nunofarruca@gmail.com
+gext install clipboard-history@alexsaveau.dev
+gext install dash-to-dock@micxgx.gmail.com
+gext install paperwm@paperwm.github.com
+gext install freon@UshakovVasilii_Github.yahoo.com
