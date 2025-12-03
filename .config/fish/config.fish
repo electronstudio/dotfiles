7c1187aa727f16b94ea332c8071df40fc441e677
Richard Smith <github@electronstudio.co.uk>
2025-12-03 09:29:14 +0000

    hmm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,12 @@ if status is-interactive
 end
 zoxide init fish | source
 starship init fish | source
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /home/richard/.lmstudio/bin
+# End of LM Studio CLI section
+
+function mkcd
+    mkdir -pv $argv
+    cd $argv
+end
