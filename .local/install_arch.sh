5fb5ced458017b6a9d3bf8774d9e27962eabd8e5
Richard Smith <github@electronstudio.co.uk>
2026-06-02 02:26:56 +0100

    update

--- .local/install_arch.sh
+++ .local/install_arch.sh
@@ -1,9 +1,14 @@
-sudo pacman -Sy eza bat ripgrep  btop fish tealdeer micro helix zoxide fzf nnn ncdu fd lf ghostty uv duf starship github-cli
+sudo pacman -Sy cmake eza bat ripgrep  btop fish tealdeer micro helix \
+	zoxide fzf nnn ncdu fd lf ghostty uv duf starship github-cli git-lfs \
+	vulkan-devel earlyoom flatpak-builder gnome gnome-extra gnome-shell
 alias fdfind fd
 alias batcat bat
+funcsave batcat
+funcsave fdfind
 echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$USER
 chsh -s /usr/bin/fish 
 sudo cp .local/earlyoom.conf /etc/default/earlyoom
 sudo systemctl enable earlyoom
 sudo systemctl start earlyoom
- 
+git config --global user.email "github@electronstudio.co.uk"
+git config --global user.name "Richard Smith" 
